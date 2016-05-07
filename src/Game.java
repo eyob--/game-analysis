d541e707d53b5469dd30052db2b911d126752d2f
eyob-- <tse.eyob@gmail.com>
2016-05-07 02:40:51 -0400

    Added more javadocs to Game

--- src/Game.java
+++ src/Game.java
@@ -3,16 +3,49 @@
  */
 public abstract class Game {
 
+	/**
+	 * Return the game's board width. Should be constant
+	 * @return board width of the game
+	 */
 	public abstract int boardWidth();
 
+	/**
+	 * Return the game's board height. Should be constant
+	 * @return board height of the game
+	 */
 	public abstract int boardHeight();
 
+	/**
+	 * Check if a player has actually won at a given state of the game
+	 * @param board board at the given state of the game
+	 * @param player player to be checked
+	 * @return true if the given player has won on the given board, false if they have not
+	 */
 	public abstract boolean won(int[][] board, int player);
 
+	/**
+	 * Gives back an array of legal moves for the player to make on the given board
+	 * @param board board at the desired state of the game
+	 * @param player player whose moves will be given back
+	 * @return array of legal moves for the given player
+	 */
 	public abstract Move[] legalMoves(int[][] board, int player);
 
+	/**
+	 * Plays a given legal move for the given player on the given board
+	 * @param board boar at the desired state of the game
+	 * @param player player who will make the move
+	 * @param legalMove the legal move to be made
+	 * @return the board after the move has been made
+	 */
 	public abstract int[][] makeMove(int[][] board, int player, Move legalMove);
 
+	/**
+	 * Whether the game can proceed or not
+	 * @param board board at the desired state of the game
+	 * @param currentPlayer player whose turn it is
+	 * @return true if the game cannot proceed, false if it still can
+	 */
 	public boolean finished(int[][] board, int currentPlayer) {
 		return won(board, 1) || won(board, 2) || legalMoves(board, currentPlayer).length == 0;
 	}
