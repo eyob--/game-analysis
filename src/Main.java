ce7d8730d327a0f41d99420d875bc1344710a75e
eyob-- <tse.eyob@gmail.com>
2016-05-07 02:46:22 -0400

    Added test case in Main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
 
 	public static void main(String[] args) {
-
+		Analyzer a = new Analyzer(new TicTacToe(3));
+		int[][] board = new int[3][3];
+		board[1][1] = 1;
+		board[0][0] = 2;
+		System.out.println(a.canWin(board, 1, 1));
 	}
 
 }
